90f54e821867b496a51e24e995d79d2ae7c1f35b
anan9399 <atroriass@gmail.com>
2023-06-27 18:18:44 +0900

    no message

--- GM31/FinishLog.cpp
+++ GM31/FinishLog.cpp
@@ -7,7 +7,7 @@ void FinishLog::Init()
 	Renderer::CreateVertexShader(m_pVertexShader.ReleaseAndGetAddressOf(), m_pInputLayout.ReleaseAndGetAddressOf(), "unlitTextureVS.cso");
 
 
-	AddComponent<Sprite>()->Init("asset\\texture\\field004.jpg", m_Position, SCREEN_HEIGHT, SCREEN_WIDTH);
+	AddComponent<Sprite>()->Init("asset\\texture\\finish1.png", m_Position, SCREEN_HEIGHT, SCREEN_WIDTH);
 	//	m_Position = D3DXVECTOR3(301.0f, 300.0f, 0.0f);
 		//AddComponent<Sprite>()->Init("asset\\texture\\field004.jpg",m_Position,2.0f,1.0f);
 }
