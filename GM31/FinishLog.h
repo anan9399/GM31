0263e6698beda9827804a692cc8982bc40e7d209
anan9399 <atroriass@gmail.com>
2023-07-01 20:32:47 +0900

    no message

--- GM31/FinishLog.h
+++ GM31/FinishLog.h
@@ -7,6 +7,16 @@ class FinishLog : public polygon2D
 {
 
 public:
-	void Init() override;
+	void Init() override 
+	{
+		Renderer::CreatePixelShader(m_pPixelShader.ReleaseAndGetAddressOf(), "unlitTexturePS.cso");
+		Renderer::CreateVertexShader(m_pVertexShader.ReleaseAndGetAddressOf(), m_pInputLayout.ReleaseAndGetAddressOf(), "unlitTextureVS.cso");
+
+
+		AddComponent<Sprite>()->Init("asset\\texture\\finish1.png", m_Position, SCREEN_HEIGHT, SCREEN_WIDTH);
+		m_Position = D3DXVECTOR3(300.0f, 300.0f, 0.0f);
+		//AddComponent<Sprite>()->Init("asset\\texture\\field004.jpg",m_Position,2.0f,1.0f);
+
+	}
 };
 
