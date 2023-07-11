657ffbc5e94229a9b6ffefeaca712be6578cd0a4
anan9399 <atroriass@gmail.com>
2023-07-11 18:02:22 +0900

    refacting a little

--- GM31/FinishLog.h
+++ GM31/FinishLog.h
@@ -18,7 +18,7 @@ public:
 		AddBind(std::make_shared<PixelShader>("unlitTexturePS.cso"));
 
 
-		AddComponent<Sprite>()->Init("asset\\texture\\finish1.png", m_Position, SCREEN_HEIGHT, SCREEN_WIDTH);
+		AddComponent<Sprite>()->Init( "asset\\texture\\finish1.png", m_Position, SCREEN_HEIGHT, SCREEN_WIDTH);
 		m_Position = D3DXVECTOR3(300.0f, 300.0f, 0.0f);
 		//AddComponent<Sprite>()->Init("asset\\texture\\field004.jpg",m_Position,2.0f,1.0f);
 
